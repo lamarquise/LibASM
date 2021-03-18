10151ac04b7b315c8fd420a0f95ca40e59e14bfe
lamarquise <e.lazo14@eabjm.org>
2021-03-18 14:36:24 +0100

    yea im not doing list sort

--- libasm_bonus.h
+++ libasm_bonus.h
@@ -23,6 +23,5 @@ typedef struct		s_list
 
 void				ft_list_push_front(t_list **begin_list, void *data);
 int					ft_list_size(t_list *begin_list);
-void				ft_list_sort(t_list **begin_list, int (*cmp)());
 
 #endif
